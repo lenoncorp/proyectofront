72aa74fb6fcc925dae9cc6841a280c7a32415427
CaroSalazarT <cst2615@gmail.com>
2021-12-14 22:15:04 -0500

    en reunión de equipo se agregan funcionalidades para el filtro de avances de acuerdo al lider

---
+++ src/pages/avances/nuevoAvance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nuevoAvance = () => {
+    return (
+        <div>
+            pagina para crear avances
+        </div>
+    )
+}
+
+export default nuevoAvance;
